12e76e684327610a5fe7fd64ee4f34849d7ed396
opnfuse <opnfuso@gmail.com>
2021-01-26 23:04:10 -0600

    Scrapper finished

--- scrapper.js
+++ scrapper.js
@@ -1,24 +1,30 @@
 const puppeteer = require('puppeteer');
+const clipr = 'https://clipr.xyz/';
 
 const scrapper = async (url) => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setDefaultTimeout(20000);
-  await page.goto(url);
-  await page.waitForSelector('video');
+  await page.goto(clipr);
+  await page.waitForSelector('#clip_url');
+  await page.type('#clip_url', url);
+  await page.click('form > button');
+  await page.waitForSelector(
+    'div.row.text-center > div:nth-child(2) > div.mt-3.text-center > a:nth-child(2)'
+  );
   await page.screenshot({ fullPage: true, path: 'screen.jpg' });
 
   const getPageData = async (page) => {
     const page_data = await page.evaluate(() => {
-      const video_url = 'hola';
+      let video_url = 'hola';
 
       try {
-        const $video = document.querySelector('video');
+        const $video = document.querySelector(
+          'div.row.text-center > div:nth-child(2) > div.mt-3.text-center > a:nth-child(2)'
+        );
 
-        video_url = 'No';
-      } catch (error) {
-        console.error(error);
-      }
+        video_url = $video.getAttribute('href');
+      } catch (error) {}
 
       return video_url;
     });
@@ -27,10 +33,11 @@ const scrapper = async (url) => {
   };
 
   const data = await getPageData(page);
+  const video_url = `https:${data}`;
 
-  // await browser.close();
+  await browser.close();
 
-  return data;
+  return video_url;
 };
 
 async function get() {
